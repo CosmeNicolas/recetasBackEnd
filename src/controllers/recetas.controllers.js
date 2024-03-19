773732a555e8d4ca6d9f4996aed5b1cd0c81e042
CosmeNicolas <nicomas7590@gmail.com>
2024-03-18 21:34:03 -0300

    agregamos la funcion obtener receta la cual utiliza un id como parametro

--- src/controllers/recetas.controllers.js
+++ src/controllers/recetas.controllers.js
@@ -12,17 +12,30 @@ export const listarRecetas = async(req, res)=>{
 
 export const crearReceta = async(req, res)=>{
     try {
-        //extraer los datos del body
-      
-        //validar los datos del body 
-        //pedir a la BD crear el producto
         const recetaNueva = new Receta(req.body);
         await recetaNueva.save();
-        //responder al frontend
         res.status(201).json(
             {mensaje:'La receta fue creada exitosamente'})
     } catch (error) {
         console.log(error)
         res.status(400).json({mensaje:'La receta no pude ser creada'})
+    }
+}
+
+export const obtenerReceta = async(req, res)=>{
+    try {
+        //extraer id 
+        console.log(req.params.id)
+        //solicitar a la bd ese id
+        const recetaBuscada = await Receta.findById(req.params.id)
+        //preguntar si no encontre el producto buscado
+        if(!recetaBuscada){
+            return res.status(404).json({mensaje: 'No se encontro la receta con el id enviado'})
+        }
+        //enviar respuesta
+        res.status(200).json(recetaBuscada)
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({mensaje: 'No se pudo encontrar el producto solicitado'})
     }
 }
--- src/routes/recetas.routes.js
+++ src/routes/recetas.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { crearReceta, listarRecetas } from "../controllers/recetas.controllers.js";
+import { crearReceta, listarRecetas, obtenerReceta } from "../controllers/recetas.controllers.js";
 
 const router = Router();
 
 router.route('/recetas').get(listarRecetas).post(crearReceta);
+router.route('/recetas/:id').get(obtenerReceta)
 export default router;
